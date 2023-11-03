80f3e2429fdfa0ba4525024b22d3a65cdfd67993
Curious Guiding Light <131623836+Guidinglight23@users.noreply.github.com>
2023-11-03 09:22:21 -0500

    Add files via upload

---
+++ boothand.py
@@ -0,0 +1,24 @@
+import requests
+
+def get_cody_response(prompt):
+    url = "https://api.codybot.ai/v1/ask"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer "
+    }
+    data = {
+        "prompt": prompt,
+        "max_tokens": 100
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    response_data = response.json()
+
+    if "choices" in response_data:
+        return response_data["choices"][0]["text"].strip()
+    else:
+        return None
+
+prompt = "Once upon a time"
+generated_text = get_cody_response(prompt)
+print(generated_text)
